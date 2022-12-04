e709a4ae0bf8aaa601654d760e4fa22a9b83f2ff
tasyanat123 <119665577+tasyanat123@users.noreply.github.com>
2022-12-04 16:05:37 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const express = require('express')
 const app = express()
+const fs = require('fs');
+const content = 'Some content!';
+
 app.all('/', (req, res) => {
     console.log("Just got a request!")
-    res.send('Yo!')
+    fs.writeFile('/test.txt', content, err => {
+        if (err) {
+          console.error(err);
+        }
+        // file written successfully
+        res.send('Yo1!')
+    });
+    res.send('Yo2!')
 })
+
+
 app.listen(process.env.PORT || 3000)
